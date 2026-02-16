fc0bfb303efde41abb5f82112989346b4f0dc984
abhinavsharma0001 <abhinav10169@gmail.com>
2026-02-16 11:41:50 +0530

    Add files via upload

---
+++ Classes/class30-01/LC1688.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+
+        
+    }
+    int main(){
+        int n;
+        cin>>n;
+        cout<< numberOfMatches( n);
+        return 0;
+    }
+};
---
+++ Classes/class30-01/LC202.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int nextnum(int n)
+{
+      int sum =0;
+     while (n > 0) 
+            {
+              
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+           return  sum;
+}
+
+    bool isHappy(int n)
+     {
+        while (n != 1 && n!=4) 
+        {
+            n=nextnum(n);
+        }
+        return n==1;
+     }
+
+};
